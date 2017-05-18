9de4e0606d1fc21be075a828aac7ab59f5cf5a41
Constantin EhrlichAdam <c.ehrlichadam@hotmail.com>
2017-05-18 11:23:03 +0100

    Seeded database with JSON file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+file = open(url).read
+
+data_hash = JSON.parse(file)
+
+data_hash["drinks"].each do |drink|
+  Ingredient.create(name: drink["strIngredient1"])
+end
